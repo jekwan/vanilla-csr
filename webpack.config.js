d21213766a5151fc418b133eee6f507b0112d0de
jekwan <jekwan@kakao.com>
2022-08-24 00:31:01 +0900

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  devServer: {
+    port: 8081,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
